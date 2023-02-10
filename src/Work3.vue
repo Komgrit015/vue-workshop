c3e3958e5b23738b881612fe2a8a4887b92398ef
Komgrit015 <ford_za007@hotmail.com>
2023-02-10 15:05:59 +0700

    Workshop 3

---
+++ src/Work3.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="Work3">
+      <div class="container">
+        
+        <h1>Workshop #3 Preview Image</h1>
+        <hr>
+     
+      </div>
+    </div>
+  </template>
+    
+    <script>
+  export default {
+    name: "Work3",
+    data() {
+      return {
+       
+      }
+    },
+  };
+  </script>
+    
+    <style>
+  #Work3 {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+    
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Work2 from './Work2.vue'
+import Work3 from './Work3.vue'
+
 
 createApp(App).mount('#app')
 createApp(Work2).mount('#Work2')
+createApp(Work3).mount('#Work3')
+
 
